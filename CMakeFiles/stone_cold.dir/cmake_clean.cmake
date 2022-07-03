804b9fd4b501b7d8e55a48956bc025e74c55d869
Peter Baumann <peterbaumann2@gmx.de>
2022-07-03 14:25:30 +0200

    Bare bones SFML setup - VS Code + CMake

---
+++ CMakeFiles/stone_cold.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stone_cold.dir/src/Application.cpp.obj"
+  "CMakeFiles/stone_cold.dir/src/Application.cpp.obj.d"
+  "x64/bin/stone_cold.exe"
+  "x64/bin/stone_cold.exe.manifest"
+  "x64/bin/stone_cold.pdb"
+  "x64/lib/libstone_cold.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stone_cold.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/stone_cold.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for stone_cold.
+# This may be replaced when dependencies are built.
